b7d9293b6c530fb4706b97c1cba0513be1f20b96
serge <serega2703200145@gmail.com>
2022-10-28 01:10:42 +0300

    Commit

--- Lab_4_2.py
+++ Lab_4_2.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import re
 
 
 def cost(df, path):
@@ -14,7 +15,22 @@ def cost(df, path):
 
 
 df_dist = pd.read_csv('ha30_dist.txt', sep='\t', header=None)
-df_dist = pd.DataFrame([x.strip().replace('   ', ' ').replace('  ', ' ').split(' ') for x in df_dist[0]])
+df_dist = pd.DataFrame([x.strip().replace('    ', ' ').replace('   ', ' ').replace('  ', ' ').split(' ') for x in df_dist[0]])
+
+df_coords = pd.read_csv('ha30_xyz.txt', sep='\t', decimal='.', header=None)
+df_coords = pd.DataFrame([x.strip().replace('       ', ' ').replace('  ', ' ').split() for x in df_coords[0]])
+
+xs = []
+ys = []
+
+for index, row in df_coords.iterrows():
+    xs.append(float(row[0]))
+    ys.append(float(row[1]))
+
+plt.plot(xs, ys, 'o-')
+plt.plot(xs[0], ys[0], 'ro')
+plt.grid()
+plt.show()
 
 with open('ha30_name.txt') as f:
     names = f.read()
@@ -28,6 +44,8 @@ df_test = df_dist.copy()
 
 cities = list(df_test.columns)
 start_city = cities[0]
+
+df_coords.index = cities
 
 cities = list(np.random.choice(cities, size=len(cities), replace=False))
 
@@ -62,6 +80,18 @@ for i in range(100000):
 
 print('best path - ', best_path)
 print('with cost - ', best_cost)
+
+xs1 = []
+ys1 = []
+
+for city in best_path.split(","):
+    xs1.append(float(df_coords.loc[city][0]))
+    ys1.append(float(df_coords.loc[city][1]))
+
+plt.plot(xs1, ys1, 'o-')
+plt.plot(xs1[0], ys1[0], 'ro')
+plt.grid()
+plt.show()
 
 cost_stats = []
 cost_max = 0
